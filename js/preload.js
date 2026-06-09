ee91afca33cf965ee41119a42c885d2f0b4f4065
Valens Kwizera <vahlens95@gmail.com>
2026-06-09 20:21:50 +0200

    Delete old files

--- js/preload.js
+++
@@ -1,9 +0,0 @@
-if (!location.href.includes("/retailer/")) {
-  document
-    .getElementById("app-icons")
-    .setAttribute("href", "/css/icons.min.css?v=3.0.0");
-} else {
-  document
-    .getElementById("retailer-theme")
-    .setAttribute("href", "/css/retailer.css");
-}
